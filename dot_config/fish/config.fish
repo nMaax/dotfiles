0bad24557cb2ac09cc2a82f48bbab6bbad0236e3
nMaax <massimilianocarli@proton.me>
2026-04-02 01:56:10 +0200

    added pdf handling functions

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,6 +9,36 @@ function fish_greeting
     end
 end
 
+function pdf-shrink
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen \
+        -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$argv[2] $argv[1]
+end
+
+function pdf-ocr
+    ocrmypdf --deskew --clean $argv[1] $argv[1]
+end
+
+alias pdf-merge="pdfcpu merge"
+alias pdf-split="pdfcpu split"
+alias pdf-pages="pdfcpu collect" # Extract specific pages: pdf-pages in.pdf out.pdf 1-3
+
+# Convert Markdown to PDF
+alias md2pdf="pandoc -o output.pdf"
+
+# Extract images from PDF into a folder
+function pdf-images
+    mkdir -p ./extracted-images
+    pdfimages -j $argv[1] ./extracted-images/img
+end
+
+# Rip all text out of a PDF
+alias pdf-to-txt="pdftotext"
+
+# Remove a password from a PDF
+function pdf-unlock
+    qpdf --password=$argv[2] --decrypt $argv[1] unlocked_$argv[1]
+end
+
 function hyperion
     # Change background color to PoliTo Deep Blue when connected to Hyperion
     printf "\e]11;#1a0b2e\a"
@@ -24,4 +54,3 @@ if status is-interactive
     # Start starship for a eye-candy terminal interaction
     starship init fish | source
 end
-
