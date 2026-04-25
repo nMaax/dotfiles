3cf1db1569f052a3b1c183cccefd77dc624b8ff9
nMaax <massimilianocarli@proton.me>
2026-04-25 23:34:35 +0200

    Removed hyperion fish function

--- dot_config/fish/functions/hyperion.fish
+++
@@ -1,7 +0,0 @@
-function hyperion
-    # Change background color to PoliTo Deep Blue when connected to Hyperion
-    printf "\e]11;#1a0b2e\a"
-    ssh hyperion
-    # Revert the color back to a default black (doesnt affect other terminal sessions)
-    printf "\e]11;#000000\a"
-end
