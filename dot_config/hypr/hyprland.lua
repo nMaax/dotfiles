f00e1effd904163c306f1941775f5a57b30c9329
Massimiliano <117172198+nMaax@users.noreply.github.com>
2026-08-03 02:05:32 +0200

    Fix typo in TODO comment
    Corrected a typo in the TODO comment regarding optimization for battery and resource consumption.

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -13,14 +13,11 @@ require("lua.plugins")
 require("lua.performance")
 require("lua.misc")
 
--- TODO: we should move many function in dedicated files and re-organize the code overall
--- TODO: also constants should be moved, ideally here, and we should find some around
 -- TODO: update all links to hyprland documentation
 -- TODO: ask for finding QoL features online
--- TODO: fix various colorings from noctalia, also for community themes (e.g. Ghostty, FastFetch, heroic etc.)
 -- TODO: make claude fix direct scanout on games, my suspicioun are, in order:
 --    - the problem may be wallpaper engine
 --    - the problem may be combination with vrr
 --    - the problem may be using scrolling mode instead of dwindle
 -- TODO: make claude find the best prefix for game launching in steam for cachyos
--- TOOD: make cluade optimize for battery and resources consumption
+-- TOOD: make claude optimize for battery and resources consumption
